0690b5a90832abcb80071d93988d46677781752b
Aleksandra_Kutishcheva <welain04@mail.ru>
2026-03-06 21:10:43 +0300

    Настройка сервера, чтобы открыть в браузере

--- server/index.ts
+++ server/index.ts
@@ -90,12 +90,13 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
+  const listenOptions = {
+    port,
+    host: "0.0.0.0",
+    ...(process.platform !== "win32" ? { reusePort: true } : {}),
+  };
   httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-      reusePort: true,
-    },
+    listenOptions,
     () => {
       log(`serving on port ${port}`);
     },
